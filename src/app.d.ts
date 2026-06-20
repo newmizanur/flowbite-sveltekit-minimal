5cc1d8698b59ae8a41d278a6cdb5b65b413d305a
Mizan <newmizanur@gmail.com>
2026-06-20 18:52:07 +0800

    feat: add Locals.user type to app.d.ts

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,9 +1,15 @@
 // See https://kit.svelte.dev/docs/types#app
-// for information about these interfaces
 declare global {
   namespace App {
+    interface Locals {
+      user: {
+        id: string;
+        name: string;
+        email: string;
+        role: string;
+      } | null;
+    }
     // interface Error {}
-    // interface Locals {}
     // interface PageData {}
     // interface PageState {}
     // interface Platform {}
